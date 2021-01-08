46bde148edd914dd3406b192c2a8efa73ac0a442
SergiooOliveira <58986379+SergiooOliveira@users.noreply.github.com>
2021-01-08 00:44:30 +0000

    Initial commit

---
+++ app/src/main/java/ipca/cm/kermit/KermitGame.kt
@@ -0,0 +1,37 @@
+package ipca.cm.kermit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.ImageView
+
+class KermitGame : AppCompatActivity() {
+
+    var x : Double? = 0.0
+    var y : Double? = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kermit_game)
+
+        var yourImage = findViewById<ImageView>(R.id.kermit)
+
+        yourImage.setOnTouchListener{view , event->
+            when (event.action){
+                MotionEvent.ACTION_DOWN -> {
+                    x = view.x.toDouble() - event.rawX
+                    true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    view.x = event.rawX - view.width/2
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/ipca/cm/kermit/MainActivity.kt
@@ -0,0 +1,24 @@
+package ipca.cm.kermit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val intent = Intent(this@MainActivity, KermitGame::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
